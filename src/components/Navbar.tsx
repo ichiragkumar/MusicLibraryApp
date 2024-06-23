ec1b440a113ee6ca2115727f9efbaec4069a689a
ichiragkumar <chiragroy3030@gmail.com>
2024-06-23 15:57:58 +0530

    Hero section added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -8,7 +8,7 @@ export const  Navbar = ({ className }: { className?: string })=> {
 
 
   return (
-    <div className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
+    <div className={cn("fixed top-10 inset-x-0 max-w-full mx-auto z-50", className)}>
 
         <Menu setActive={setActive}>
 
@@ -16,7 +16,7 @@ export const  Navbar = ({ className }: { className?: string })=> {
                 <MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
             </Link>
 
-            <MenuItem setActive={setActive} active={active} item="Our courses">
+            <MenuItem setActive={setActive} active={active} item="Courses">
                <div className="flex flex-col space-y-4 text-sm ">
                <HoveredLink href="/courses">All Courses</HoveredLink>
                <HoveredLink href="/courses">Basic Music Library</HoveredLink>
@@ -26,8 +26,16 @@ export const  Navbar = ({ className }: { className?: string })=> {
                </div>
 
             </MenuItem>
-           <Link href={"/contact"} className="text-white">
+           <Link href={"/contact"} >
                 <MenuItem setActive={setActive} active={active} item="Contact us"></MenuItem>
+           </Link>
+
+           <Link href={"/contact"}>
+                <MenuItem setActive={setActive} active={active} item="Pricing"></MenuItem>
+           </Link>
+
+           <Link href={"/contact"}>
+                <MenuItem setActive={setActive} active={active} item="Blogs"></MenuItem>
            </Link>
 
         </Menu>
