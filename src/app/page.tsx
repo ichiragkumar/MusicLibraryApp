6f600af6e6646bdf3ca866bf71c2ac85f250121e
ichiragkumar <chiragroy3030@gmail.com>
2024-06-23 16:38:34 +0530

    added moving border

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import { Navbar } from "@/components/Navbar";
 import HeroSection from "@/components/HeroSection";
+import { Spotlight } from "@/components/ui/Spotlight";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
+  
     <HeroSection />
   </main>
   );
